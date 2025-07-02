5967060d2c881c34063be329e72ab40f1829d11c
Andrei <askrashennikov@gmail.com>
2025-07-02 12:07:55 +0300

    fixed 09-json-api-fetch

--- 09-json-api-fetch/json-api-server.js
+++ 09-json-api-fetch/json-api-server.js
@@ -1,26 +1,11 @@
 const http = require("http"); // для створення сервера
-const fs = require("fs"); // для читання статичних файлів
-const path = require("path"); // для роботи з шляхами
+const serveStaticFile = require("./utils/serveStaticFile");
 
 const server = http.createServer((req, res) => {
     // Тут буде вся логіка: обробка GET, POST, 404
     if (req.method === "GET") {
         // Статичні файли
-        const filePath = req.url === "/" ? "/index.html" : req.url;
-        const fullPath = path.join(__dirname, "public", filePath);
-
-        fs.readFile(fullPath, (err, data) => {
-            if (err) {
-                res.writeHead(404);
-                return res.end("Not found");
-            }
-            // MIME-типи: визначаємо, чи це JS або HTML
-            const ext = path.extname(fullPath);
-            const mime = ext === ".js" ? "application/javascript" : "text/html";
-
-            res.writeHead(200, { "Content-Type": `${mime}; charset=utf-8` });
-            res.end(data);
-        });
+        serveStaticFile(req, res);
     } else if (req.method === "POST" && req.url === "/api/message") {
         let body = "";
 
